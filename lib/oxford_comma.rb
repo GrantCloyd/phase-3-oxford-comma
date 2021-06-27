c998fff264598d419dc00befc4a92b5faceaaef9
Grant Cloyd <dgcloyd@gmail.com>
2021-06-27 07:33:27 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    array.first
+  elsif array.size == 2
+    array.first + " and " + array.last
+  elsif array.size >= 3
+    array[0,(array.length-1 )].join(", ") + ", and " + array.last 
+     
+  end
 end
+
+
